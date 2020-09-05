2fcedb214525fc1d253e695382632116eb425765
sandeepraj <sandeepraj.tandon@gov.bc.ca>
2020-09-04 17:01:57 -0700

    fixing liniting issues, ran npm install with latest nr-pipeline-ext dependency

--- src/commands/functionaltest.ts
+++ src/commands/functionaltest.ts
@@ -1,8 +1,19 @@
-import {BaseCommand} from '../base'
-import {FlagNames, flagConfigScript, flagGitRemoteName, flagGitRemoteUrl, flagGitBranch, flagGitBranchRemote, flagEnvSpec, flagPullRequestNumberSpec, applyFlagDefaults,  loadConfigScript} from '../flags'
+import { BaseCommand } from "../base";
+import {
+  FlagNames,
+  flagConfigScript,
+  flagGitRemoteName,
+  flagGitRemoteUrl,
+  flagGitBranch,
+  flagGitBranchRemote,
+  flagEnvSpec,
+  flagPullRequestNumberSpec,
+  applyFlagDefaults,
+  loadConfigScript,
+} from "../flags";
 
 export default class FunctionalTest extends BaseCommand {
-  static description = 'command to run functional tests for projects'
+  static description = "command to run functional tests for projects";
 
   static flags = {
     [FlagNames.CONFIG_SCRIPT]: flagConfigScript,
@@ -12,13 +23,13 @@ export default class FunctionalTest extends BaseCommand {
     [FlagNames.GIT_REMOTE_URL]: flagGitRemoteUrl,
     [FlagNames.GIT_BRANCH]: flagGitBranch,
     [FlagNames.GIT_BRANCH_REMOTE]: flagGitBranchRemote,
-  }
+  };
 
   async run() {
-    const {flags} = this.parse(FunctionalTest)
-    await applyFlagDefaults(flags)
-    const settings = loadConfigScript(flags)
-    const {BasicFunctionalTester} = require('nr-pipeline-ext')
-    new BasicFunctionalTester(settings).runFunctionalTests()
+    const { flags } = this.parse(FunctionalTest);
+    await applyFlagDefaults(flags);
+    const settings = loadConfigScript(flags);
+    const { BasicFunctionalTester } = require("nr-pipeline-ext");
+    new BasicFunctionalTester(settings).runFunctionalTests();
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-import {BasicBuilder, BasicDeployer, BasicJavaApplicationBuilder, BasicJavaApplicationDeployer, BasicFunctionalTester} from 'nr-pipeline-ext'
-import {OpenShiftClient} from '@bcgov/pipeline-cli'
+import {
+  BasicBuilder,
+  BasicDeployer,
+  BasicJavaApplicationBuilder,
+  BasicJavaApplicationDeployer,
+  BasicFunctionalTester,
+} from "nr-pipeline-ext";
+import { OpenShiftClient } from "@bcgov/pipeline-cli";
 
-export {OpenShiftClient, BasicBuilder, BasicDeployer, BasicJavaApplicationBuilder, BasicJavaApplicationDeployer,BasicFunctionalTester}
+export {
+  OpenShiftClient,
+  BasicBuilder,
+  BasicDeployer,
+  BasicJavaApplicationBuilder,
+  BasicJavaApplicationDeployer,
+  BasicFunctionalTester,
+};
 
-export {run} from '@oclif/command'
+export { run } from "@oclif/command";
