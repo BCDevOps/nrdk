70b7d1a10f3171c0de24cf1b08724d1671ef1611
Clay Hobson <clay.hobson.17@gmail.com>
2021-01-05 15:20:23 -0800

    forgotten file

---
+++ src/api/service/axios-client.ts
@@ -0,0 +1,27 @@
+import {AxiosInstance} from 'axios'
+import axios from 'axios'
+
+export const FIELDS = Object.freeze({
+  ISSUE_TYPE: 'issuetype',
+})
+
+export interface RepositoryReference {
+    cwd?: string;
+    url?: string;
+    slug: string;
+    project: {key: string};
+  }
+
+export class AxiosClient {
+    readonly client: AxiosInstance
+
+    constructor(clientURL: string, idirAuthorizationHeader: any) {
+        this.client = axios.create({
+            baseURL: clientURL,
+            timeout: 10000,
+            headers: {
+              Authorization: idirAuthorizationHeader,
+            },
+          })
+    }
+}
