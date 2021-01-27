275c06abbc268e82aec56e9d1a9d14ea50dfc8b6
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2021-01-27 09:14:05 -0800

    Added ansible support

---
+++ src/tools/ansible.ts
@@ -0,0 +1,102 @@
+import {Tool} from './tool'
+import * as path from 'path'
+import {ChildProcess, SpawnOptions} from 'child_process'
+import {LoggerFactory} from '../util/logger'
+import {_spawn2, waitAndBuffer, waitToExit} from '../util/child-process'
+import * as os from 'os'
+import * as fs from 'fs'
+
+export class Ansible extends Tool {
+  static logger = LoggerFactory.createLogger(Ansible)
+
+  public ansible_bin_run_command = 'ansible'
+
+  async install(version: string): Promise<string> {
+    // python3 -m pip show virtualenv
+    // python3 -m pip install --user virtualenv
+    const homeDirectory = await this.getHomeDirectory('ansible', version)
+    return _spawn2(path.join(homeDirectory, 'bin', 'python3'), ['--version'])
+    .then(waitAndBuffer)
+    .then(proc => {
+      if (proc.status  !== 0) {
+        return _spawn2('python3', ['-m', 'pip', 'show', 'virtualenv'])
+        .then(waitAndBuffer)
+        .then(proc => {
+          if (proc.status  !== 0) throw new Error('virtualenv module is not installed. Please install virtualenv')
+          Ansible.logger.info('Creating python virtual environment')
+          return _spawn2('python3', ['-m', 'virtualenv', homeDirectory])
+          .then(waitAndBuffer)
+          .then(proc => {
+            if (proc.status  !== 0) throw new Error(`Error creating python virtual envvironment.\nexit code:${proc.status}\nstdout:${proc.stdout}\nstderr:${proc.stderr}`)
+            return proc
+          })
+        })
+      }
+    })
+    .then(() => {
+      return _spawn2(path.join(homeDirectory, 'bin', 'python3'), ['-m', 'pip', 'show', 'ansible'], {stdio: 'ignore'})
+      .then(waitToExit)
+      .then(proc => {
+        if (proc.status  === 0) return proc
+        Ansible.logger.info(`Installing Ansible ${version}`)
+        return _spawn2(path.join(homeDirectory, 'bin', 'python3'), ['-m', 'pip', 'install', `ansible==${version}`], {stdio: ['ignore', process.stdout, process.stderr]})
+        .then(waitToExit)
+        .then(proc => {
+          if (proc.status  !== 0) throw new Error(`Error installling ansible.\nexit code:${proc.status}`)
+          return proc
+        })
+      })
+    })
+    .then(() => {
+      return homeDirectory
+    })
+  }
+
+  async run(args: readonly string[], options?: SpawnOptions): Promise<ChildProcess> {
+    return this.run_native(args, options)
+  }
+
+  async run_native(args: readonly string[], options?: SpawnOptions): Promise<ChildProcess> {
+    return this.install('2.10.5')
+    .then(homeDirectory => {
+      Ansible.logger.debug(`Running Ansible from ${homeDirectory}`)
+      const env = {...process.env, ANSIBLE_ROLES_PATH: './.roles'}
+      return _spawn2(path.join(homeDirectory, 'bin', 'python3'), [path.join(homeDirectory, 'bin', this.ansible_bin_run_command), ...args], {...options, env})
+    })
+  }
+
+  async run_in_docker(args: readonly string[], options?: SpawnOptions): Promise<ChildProcess> {
+    const version = '2.10.5'
+    // const homeDirectory = await this.getHomeDirectory('ansible', version)
+    const dockerImageTag = `nrdk/ansible:${version}`
+    const dockerContainerIdFile  = path.join(os.tmpdir(), `nrdk-ansible-${version}.cid`)
+    return _spawn2('docker', ['build', '-t', dockerImageTag, '--build-arg', `ANSIBLE_VERSION=${version}`, path.join(__dirname, 'ansible')])
+    .then(waitAndBuffer)
+    .then(proc => {
+      if (proc.status  !== 0) throw new Error(`Error running docker build.\nexit code:${proc.status}\nstdout:${proc.stdout}\nstderr:${proc.stderr}`)
+      return _spawn2('docker', ['run', '-it', '-d=true', '--cidfile', dockerContainerIdFile, '-v', `${process.cwd()}:/workdir`, '-w', '/workdir', dockerImageTag, '/bin/sh'])
+    })
+    .then(waitToExit)
+    .then(() => {
+      const containerId = fs.readFileSync(dockerContainerIdFile, {encoding: 'utf8'})
+      // docker exec -ti "$(< container.cid)" git config --global url."https://bwa.nrs.gov.bc.ca".insteadOf https://apps.nrs.gov.bc.ca
+      // docker exec -ti "$(< container.cid)" git config --global credential.helper 'cache --timeout 3600'
+      // echo -e 'protocol=https\nhost=bwa.nrs.gov.bc.ca\nusername=XYZ\npassword=XXX' | docker exec -ti "$(< container.cid)" git credential approve
+      return _spawn2('docker', ['exec', '-w', '/workdir', containerId, '/opt/ansible/bin/python3', path.posix.join('/opt/ansible/bin', this.ansible_bin_run_command), ...args], options)
+      .then(proc => {
+        proc.on('exit', async () => {
+          return _spawn2('docker', ['rm', '--force', containerId])
+          .then(waitToExit)
+          .then(() => {
+            fs.unlinkSync(dockerContainerIdFile)
+            return proc
+          })
+        })
+        return proc
+      })
+    })
+    .then(proc => {
+      return proc
+    })
+  }
+}
--- src/util/child-process.ts
+++ src/util/child-process.ts
@@ -1,5 +1,8 @@
 import {spawn, SpawnOptions, SpawnSyncReturns, ChildProcess} from 'child_process'
 import winston from 'winston'
+import {LoggerFactory} from '../util/logger'
+
+const logger = LoggerFactory.createLogger('child-process')
 
 export async function _spawn(logger: winston.Logger, command: string, argsv: readonly string[], options?: SpawnOptions): Promise<SpawnSyncReturns<string>> {
   logger.child({group: ['exec', command], args: argsv}).debug('%s %s', command, (argsv || []).join(' '))
@@ -22,6 +25,38 @@ export async function _spawn(logger: winston.Logger, command: string, argsv: rea
   })
 }
 
+export async function _spawn2(command: string, argsv: readonly string[], options?: SpawnOptions): Promise<ChildProcess> {
+  logger.child({group: ['spawn', command], args: argsv}).debug('%s %s', command, (argsv || []).join(' '))
+  return new Promise(resolve => {
+    const child = spawn(command, argsv, options || {})
+    resolve(child)
+  })
+}
+export async function waitAndBuffer(child: ChildProcess): Promise<SpawnSyncReturns<string>> {
+  return new Promise(resolve => {
+    let stdout = ''
+    let stderr = ''
+    if (!child.stdout) throw new Error('Null stdout property!')
+    if (!child.stderr) throw new Error('Null stderr property!')
+    child.stdout.on('data', data => {
+      stdout += data
+    })
+    child.stderr.on('data', data => {
+      stderr += data
+    })
+    child.on('exit', status => {
+      resolve({pid: 0, status: status as number, stdout, stderr, output: (null as unknown) as string[], signal: null})
+    })
+  })
+}
+
+export async function waitToExit(proc: ChildProcess): Promise<SpawnSyncReturns<string>> {
+  return new Promise(resolve => {
+    proc.on('exit', status => {
+      resolve({pid: 0, status: status as number, stdout: (null as unknown) as string, stderr: (null as unknown) as string, output: (null as unknown) as string[], signal: null})
+    })
+  })
+}
 export async function waitForSuccessfulExitCode(proc: ChildProcess) {
   return new Promise((resolve, reject) => {
     proc.on('exit', exitCode => {
