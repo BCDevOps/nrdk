16b7ee2e3399027b2995135e2d23bec20468612c
Derek Roberts <derek.roberts@gmail.com>
2021-02-02 16:51:31 -0800

    greetings.sh example script for greetings command

---
+++ greetings.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -l
+set -euo nounset
+#
+# Test hello command
+
+# Link/symlink
+[ -f /usr/local/bin/nrdk ] || sudo npm link
+
+# Run watcher in background
+docker-compose up -d watcher
+
+# Wait (yes, I know this is daft)
+sleep 20
+
+# Run commands
+npx @bcgov/nrdk greetings
+npx @bcgov/nrdk greetings Earth
+npx @bcgov/nrdk greetings -n Earth
+docker-compose build
+
+# Test
+npm run mocha -- "test/commands/greetings.test.ts"
