98cc849f61c2356fbc0802ca40790810ff9f6c24
Derek Roberts <derek.roberts@gmail.com>
2021-02-08 10:53:41 -0800

    Remove Docker-Compose from demo (not necessary)

--- greetings.sh
+++ greetings.sh
@@ -3,25 +3,18 @@ set -euo nounset
 #
 # Test hello command
 
-# Link/symlink
-[ -f /usr/local/bin/nrdk ] || sudo npm link
-
-# Run watcher in background
-docker-compose up -d watcher
-
-# Wait (yes, I know this is daft)
-sleep 20
+# Build and link
+npm i -q
+npm run build
+[ -L /usr/local/bin/nrdk ] || npm link || sudo npm link
 
 # Run commands
 npx @bcgov/nrdk greetings
-npx @bcgov/nrdk greetings Earth
-npx @bcgov/nrdk greetings -n Earth
+npx @bcgov/nrdk greetings argument
+npx @bcgov/nrdk greetings -n flag
 
 # Test
 npm run mocha -- "test/commands/greetings.test.ts"
 
 # Breakpoints
-node --inspect @bcgov/nrdk greetings Earth
-
-# Stop container
-docker-compose down
+node --inspect ./bin/run greetings debug
