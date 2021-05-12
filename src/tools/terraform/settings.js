33e097277e43bc892e5914742d42fa137137efec
Derek Roberts <derek.roberts@gmail.com>
2021-05-12 12:54:01 -0700

    terraform settings.js - revise and simplify object

--- src/tools/terraform/settings.js
+++ src/tools/terraform/settings.js
@@ -1,16 +1,20 @@
 const VERSION = '0.15.3'
-const DL_PATH = 'https://releases.hashicorp.com/terraform'
+const DL_PATH = `https://releases.hashicorp.com/terraform/${VERSION}`
 
 module.exports = {
   version: VERSION,
   binary: {
-    linux: `${DL_PATH}/${VERSION}/terraform_${VERSION}_linux_amd64.zip`,
-    macos: `${DL_PATH}/${VERSION}/terraform_${VERSION}_darwin_amd64.zip`,
-    windows: `${DL_PATH}/${VERSION}/terraform_${VERSION}_windows_amd64.zip`,
-  },
-  destination: {
-    linux: '/usr/local/bin',
-    macos: '/usr/local/bin',
-    windows: '/usr/local/bin',
+    linux: {
+      dest: '/usr/local/bin',
+      link: `${DL_PATH}/terraform_${VERSION}_linux_amd64.zip`,
+    },
+    macos: {
+      dest: '/usr/local/bin',
+      link: `${DL_PATH}/terraform_${VERSION}_darwin_amd64.zip`,
+    },
+    windows: {
+      dest: '/usr/local/bin',
+      link: `${DL_PATH}/terraform_${VERSION}_windows_amd64.zip`,
+    },
   },
 }
