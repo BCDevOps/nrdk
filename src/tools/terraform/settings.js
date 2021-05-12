733515313e3baa1936c5b38ed2b69879ea179023
Derek Roberts <derek.roberts@gmail.com>
2021-05-12 15:06:40 -0700

    Use platform type when providing terraform settings

--- src/tools/terraform/settings.js
+++ src/tools/terraform/settings.js
@@ -1,20 +1,46 @@
-const VERSION = '0.15.3'
-const DL_PATH = `https://releases.hashicorp.com/terraform/${VERSION}`
+const version = '0.15.3'
+const download = `https://releases.hashicorp.com/terraform/${version}`
+const platform = require('os').platform()
+
+const binaries = {
+  detected: platform,
+  linux: {
+    dest: '/usr/local/bin',
+    link: `${download}/terraform_${version}_linux_amd64.zip`,
+  },
+  macos: {
+    dest: '/usr/local/bin',
+    link: `${download}/terraform_${version}_darwin_amd64.zip`,
+  },
+  windows: {
+    dest: '/usr/local/bin',
+    link: `${download}/terraform_${version}_windows_amd64.zip`,
+  },
+}
+
+function installer() {
+  const toReturn = {
+    app: 'Terraform',
+    platform: platform,
+    version: version,
+  }
+  switch (platform) {
+  case 'linux':
+    toReturn.binary = binaries.linux
+    return toReturn
+  case 'darwin':
+    toReturn.binary = binaries.macos
+    return toReturn
+  case 'win32':
+    toReturn.binary = binaries.windows
+    return toReturn
+  default:
+    return 'unknown'
+  }
+}
 
 module.exports = {
-  version: VERSION,
-  binary: {
-    linux: {
-      dest: '/usr/local/bin',
-      link: `${DL_PATH}/terraform_${VERSION}_linux_amd64.zip`,
-    },
-    macos: {
-      dest: '/usr/local/bin',
-      link: `${DL_PATH}/terraform_${VERSION}_darwin_amd64.zip`,
-    },
-    windows: {
-      dest: '/usr/local/bin',
-      link: `${DL_PATH}/terraform_${VERSION}_windows_amd64.zip`,
-    },
-  },
+  version: version,
+  binary: binaries,
+  installer: installer(),
 }
