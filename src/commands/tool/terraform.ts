0858a7879c117cc9f045a846fb507e0860be27b1
Derek Roberts <derek.roberts@gmail.com>
2021-05-15 10:56:12 -0700

    terraform.ts +basic cmds (init, validate, plan, apply, destroy

--- src/commands/tool/terraform.ts
+++ src/commands/tool/terraform.ts
@@ -14,10 +14,15 @@ export default class ToolTerraform extends Command {
   static hidden = true
 
   static flags = {
-    help: flags.help({char: 'h', description: 'terraform wrapper help'}),
-    install: flags.boolean({char: 'i', description: 'install terraform'}),
-    remove: flags.boolean({char: 'r', description: 'remove terraform'}),
-    version: flags.boolean({char: 'v', description: 'terraform version'}),
+    help: flags.help({char: 'h', description: 'Terraform wrapper help'}),
+    install: flags.boolean({char: 'i', description: 'Install terraform'}),
+    remove: flags.boolean({char: 'r', description: 'Remove terraform'}),
+    version: flags.boolean({char: 'v', description: 'Terraform version'}),
+    init: flags.boolean({description: 'Prepare your working directory for other commands'}),
+    validate: flags.boolean({description: 'Check whether the configuration is valid'}),
+    plan: flags.boolean({description: 'Show changes required by the current configuration'}),
+    apply: flags.boolean({description: 'Create or update infrastructure'}),
+    destroy: flags.boolean({description: 'Destroy previously-created infrastructure'}),
   }
 
   static strict = false
@@ -26,14 +31,23 @@ export default class ToolTerraform extends Command {
     const {flags} = this.parse(ToolTerraform)
     console.log('')
 
-    // Installer object contains name, platform, version and binary path
     const tf = new Terraform()
     if (flags.install) {
       tf.install()
     } else if (flags.remove) {
       tf.remove()
     } else if (flags.version) {
-      tf.run(['-v'])
+      tf.run(['version'])
+    } else if (flags.apply) {
+      tf.run(['apply'])
+    } else if (flags.destroy) {
+      tf.run(['destroy'])
+    } else if (flags.init) {
+      tf.run(['init'])
+    } else if (flags.plan) {
+      tf.run(['plan'])
+    } else if (flags.validate) {
+      tf.run(['validate'])
     } else {
       console.log('Please run with the flag --help')
     }
