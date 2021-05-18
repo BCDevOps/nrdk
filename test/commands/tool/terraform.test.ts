b1d7ecd49a4cc45a0383d05c0d5702b45fa481b1
Derek Roberts <derek.roberts@gmail.com>
2021-05-17 20:30:27 -0700

    Add to tests

--- test/commands/tool/terraform.test.ts
+++ test/commands/tool/terraform.test.ts
@@ -5,30 +5,41 @@ describe('tool:terraform', () => {
   .stdout()
   .command(['tool:terraform'])
   .it('runs tool:terraform', ctx => {
-    expect(ctx.stdout).to.contain('Please run with the flag --help')
+    expect(ctx.stdout).to.contain('Show this help output, or the help for a specified subcommand.')
   })
 
   test
   .stdout()
-  .command(['tool:terraform', '-h'])
-  .exit(0)
+  .command(['tool:terraform', '-help'])
   .it('runs terraform:tool help', ctx => {
-    expect(ctx.stdout).to.contain('terraform wrapper help')
+    expect(ctx.stdout).to.contain('Show this help output, or the help for a specified subcommand.')
   })
 
   test
   .stdout()
-  .command(['tool:terraform', '-c', '--', '--version'])
-  .exit(0)
-  .it('runs tool:terraform -c -- --version', ctx => {
+  .command(['tool:terraform', '-version'])
+  .it('runs tool:terraform -version', ctx => {
     expect(ctx.stdout).to.match(/\nTerraform v\d+\.\d+\.\d+/)
   })
 
   test
   .stdout()
-  .command(['tool:terraform', '-v'])
-  .exit(0)
-  .it('runs tool:terraform -v', ctx => {
-    expect(ctx.stdout).to.match(/\nTerraform v\d+\.\d+\.\d+/)
+  .command(['tool:terraform', 'init'])
+  .it('runs terraform:tool init', ctx => {
+    expect(ctx.stdout).to.contain('Terraform initialized in an empty directory!')
+  })
+
+  test
+  .stderr()
+  .command(['tool:terraform', 'plan'])
+  .it('runs terraform:tool plan', ctx => {
+    expect(ctx.stderr).to.contain('Error: No configuration files')
+  })
+
+  test
+  .stdout()
+  .command(['tool:terraform', 'validate'])
+  .it('runs terraform:tool validate', ctx => {
+    expect(ctx.stdout).to.contain('Success! The configuration is valid.')
   })
 })
