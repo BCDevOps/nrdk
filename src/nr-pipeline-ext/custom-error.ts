786e29b04ad61a5254430855ed8968fe68b80618
Derek Roberts <derek.roberts@gmail.com>
2021-05-26 17:12:44 -0700

    custom-error.ts copied and converted

---
+++ src/nr-pipeline-ext/custom-error.ts
@@ -0,0 +1,26 @@
+export class BasicCustomError extends Error {
+  data: string
+
+  date: Date
+
+  name: string
+
+  constructor(data: string, ...params: string[]) {
+    // Pass remaining arguments (including vendor specific ones) to parent constructor
+    super(...params)
+
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, BasicCustomError)
+    }
+
+    this.name = 'BasicCustomError'
+    // Custom debugging information
+    this.data = data
+    this.date = new Date()
+  }
+}
+
+// Add other CustomeError(s) if needed.
+
+module.exports = {BasicCustomError}
