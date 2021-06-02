b1e61b3ec133f99bb71105126c53bbea3a84620a
Derek Roberts <derek.roberts@gmail.com>
2021-06-02 10:35:36 -0700

    lib.test.ts - fixed bug emptying params

--- test/pipeline-cli/lib.test.ts
+++ test/pipeline-cli/lib.test.ts
@@ -1,15 +1,15 @@
 import * as fs from 'fs'
 
-module.exports = class LibTest {
+export class LibTest {
+  // Process template, params from args and template
   static process(filePath: string, args: any) {
     let template = fs.readFileSync(filePath, {encoding: 'utf-8'})
     const templateAsJson = JSON.parse(template)
     const params = Object.assign({}, args.param || {})
-
+    // Pick up additional params from template.parameters
     templateAsJson.parameters.forEach((p: any) => {
-      if (params[p.name] !== null && p.value !== null) {
-        params[p.name] = p.value
-      }
+      // If param not already present, then add it
+      if (!params[p.name] && p.value !== undefined) params[p.name] = p.value
     })
 
     Object.keys(params).forEach(prop => {
